70d198086350cccc4c7eb2d681545bd38473caa2
Preetha <palanisamypreetha500@gmail.com>
2026-09-09 03:12:30 -0700

    main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.database import Base, engine
 from app.models.customer import Customer
 from app.models.customer_address import CustomerAddress
+from app.models.category import Category
 
 # routers
 
 from app.routers import customer
 from app.routers import address
+from app.routers import category
 
 
 app = FastAPI(
@@ -16,7 +19,16 @@ app = FastAPI(
 )
 
 
+# Static files
+app.mount(
+    "/uploads",
+    StaticFiles(directory="uploads"),
+    name="uploads"
+)
+
+
 
 # Include routers
 app.include_router(customer.router)
 app.include_router(address.router)
+app.include_router(category.router)
