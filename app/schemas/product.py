4a5db177cdf49d8311d0bc762295f69a7d43c76a
Preetha <palanisamypreetha500@gmail.com>
2026-09-16 23:12:01 -0700

    product.py

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -2,51 +2,46 @@ from decimal import Decimal
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel,ConfigDict
+from pydantic import BaseModel, ConfigDict
+
 
 class ProductCreate(BaseModel):
-    subcategory_id : int
-    unit_id : int
-    name : str
-    sku : str
-    display_order : int = 0
-    description : Optional[str] = None
-    price : Decimal
-    mrp : Optional[Decimal] = None
+    subcategory_id: int
+    unit_id: int
+    name: str
+    sku: str
+    display_order: int = 0
+    description: Optional[str] = None
+    price: Decimal
+    mrp: Optional[Decimal] = None
     discount_price: Optional[Decimal] = None
-    tax_percentage : Decimal = Decimal("0")
-    is_available : bool = True
-    is_active : bool = True
+    tax_percentage: Decimal = Decimal("0")
+    is_available: bool = True
+    is_active: bool = True
 
-# class ProductResponse(BaseModel):
-#     id: int
-#     subcategory_id: int
-#     unit_id: int
-#     name: str
-#     slug: str
-#     description: Optional[str] = None
-#     sku: str
-#     price: Decimal
-#     mrp: Optional[Decimal] = None
-#     discount_price: Optional[Decimal] = None
-#     tax_percentage: Decimal
-#     is_available: bool
-#     is_active: bool
-#     display_order: int
-#     created_at: datetime
-#     updated_at: datetime
 
-#     model_config = ConfigDict(from_attributes=True)
+# ==========================================================
+# CATEGORY RESPONSE
+# ==========================================================
+
 class CategoryResponse(BaseModel):
     id: int
     name: str
 
+
+# ==========================================================
+# SUBCATEGORY RESPONSE
+# ==========================================================
 
 class SubcategoryResponse(BaseModel):
     id: int
     name: str
     category: CategoryResponse
 
+
+# ==========================================================
+# PRODUCT RESPONSE
+# ==========================================================
 
 class ProductResponse(BaseModel):
     id: int
@@ -63,8 +58,9 @@ class ProductResponse(BaseModel):
     is_available: bool
     is_active: bool
     display_order: int
-    created_at: datetime
-    updated_at: datetime
+
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
     subcategory: Optional[SubcategoryResponse] = None
 
